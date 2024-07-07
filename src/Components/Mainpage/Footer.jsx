99cad4462cfdf5dbcc6c9922c96bdea5993fde8b
Mayank Paliwal <pali28913@gmail.com>
2024-07-07 16:04:25 +0530

    littlechanges

--- src/Components/Mainpage/Footer.jsx
+++ src/Components/Mainpage/Footer.jsx
@@ -4,33 +4,33 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <div className=' py-10 sm:py-12 md:py-14 px-8 sm:px-10 md:px-12 bg-black border-b-8  ml-[15%] sm:ml-[10%] border-[#D29A5A]  block sm:flex justify-between'>
-        <div className='flex gap-2 sm:gap-6 my-4 sm:my-0 '>
-            <div className=''>
+    <div className=' py-10 sm:py-12 md:py-14 px-8 sm:px-10 md:px-12 bg-black border-b-8  ml-[15%] sm:ml-[10%] border-[#D29A5A]  block sm:gap-4 sm:flex justify-between'>
+        <div className='flex flex-1 gap-2 sm:gap-6 my-4 sm:my-0 '>
+            <div className='w-5 sm:w-6 md:w-8 lg:w-9'>
                 <img src="./assets/bag.png" alt="" className="" />
             </div>
             <div> 
-                <p className=" text-lg sm:text-xl md:text-2xl  text-[#D29A5A] pt-1">FREE SHIPPING WORLDWIDE</p>
+                <p className=" text-lg sm:text-xl md:text-2xl  text-[#D29A5A]  md:pt-1">FREE SHIPPING WORLDWIDE</p>
                 <p className="text-white text-xs sm:text-base md:text-lg pt-3">Guaranteed Delivery</p>
             </div>
         </div>
 
-        <div className='flex  gap-2 sm:gap-6 my-4 sm:my-0'>
-            <div className=''>
-                <img src="./assets/bag.png" alt="" className="" />
+        <div className='flex flex-1  gap-2 sm:gap-6 my-4 sm:my-0'>
+            <div className='w-5 sm:w-6 md:w-8 lg:w-9'>
+                <img src="./assets/headphone.png" alt="" className="" />
             </div>
             <div> 
-            <p className=" text-lg sm:text-xl md:text-2xl  text-[#D29A5A] pt-1">FREE SHIPPING WORLDWIDE</p>
-            <p className="text-white text-xs sm:text-base md:text-lg pt-3">Guaranteed Delivery</p>
+            <p className=" text-lg sm:text-xl md:text-2xl  text-[#D29A5A]  md:pt-1">24/7 CUSTOMER SERVICE</p>
+            <p className="text-white text-xs sm:text-base md:text-lg pt-3">Text Us 24/7 at 070-7782-9137</p>
             </div>
         </div>
-        <div className='flex  gap-2 sm:gap-6 my-4 sm:my-0'>
-            <div className=''>
-                <img src="./assets/bag.png" alt="" className="" />
+        <div className='flex flex-1  gap-2 sm:gap-6 my-4 sm:my-0'>
+            <div className='w-5 sm:w-6 md:w-8 lg:w-9'>
+                <img src="./assets/tick.png" alt="" className="" />
             </div>
             <div> 
-            <p className=" text-lg sm:text-xl md:text-2xl  text-[#D29A5A] pt-1">FREE SHIPPING WORLDWIDE</p>
-            <p className="text-white text-xs sm:text-base md:text-lg pt-3">Guaranteed Delivery</p>
+            <p className=" text-lg sm:text-xl md:text-2xl  text-[#D29A5A] md:pt-1">QUALITY GUARANTEE!</p>
+            <p className="text-white text-xs sm:text-base md:text-lg pt-3">Send Within 30 Days</p>
             </div>
         </div>
       
--- src/Components/Mainpage/Subscribesave.jsx
+++ src/Components/Mainpage/Subscribesave.jsx
@@ -19,15 +19,15 @@ const Subscribesave = () => {
             className="w-full min-h-[250px] md:min-h-[300px] lg:min-h-full"
           />
           <div className=" w-full flex absolute bottom-[10%] z-10 flex-col pl-8 sm:pl-10 md:pl-12" >
-           <div className="w-[45%] border-white border-b-4">
+           <div className=" w-3/4 sm:w-[45%] border-white border-b-4">
            <p className="pb-4 text-white text-xl  sm:text-2xl md:text-3xl">SIGN UP SPECIAL PROMOTIONS</p>
            </div>
             <p className="text-xs sm:text-base md:text-lg pt-4  sm:pt-6 md:pt-8 text-white">
             Get exclusive deals you won’t find anywhere else straight to your inbox!
             </p>
             <div className="w-full flex gap-3 pt-4 sm:pt-6 md:pt-8">
-                <input type="text" placeholder="Enter email address" className="w-[40%] p-2 text-xs sm:text-base md:text-lg bg-transparent outline-none border-2 border-white text-white" />
-                <button className="px-6 sm:px-10 py-2 text-xs sm:text-base md:text-lg bg-[#D29A5A]">Subsribe</button>
+                <input type="text" placeholder="Enter email address" className="w-1/2 sm:w-[40%] p-2 text-xs sm:text-base md:text-lg bg-[#ffffff4d] outline-none border-2 border-white text-white" />
+                <button className="px-6 sm:px-10 py-2 text-xs sm:text-base md:text-lg bg-[#D29A5A]"><p className="text-white">Subsribe</p></button>
             </div>
           </div>
         </div>
